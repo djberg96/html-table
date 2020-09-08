da9b2ed737f823e4e5c1159d56d53c72abc3c10f
Daniel Berger <djberg96@gmail.com>
2020-09-08 17:07:12 -0400

    Update foot specs.

--- spec/foot_spec.rb
+++ spec/foot_spec.rb
@@ -1,43 +1,37 @@
 ###############################################################################
-# test_foot.rb
+# foot_spec.rb
 #
-# Test suite for the Table::Foot class. The Table::Foot class is a singleton
+# Test suite for the HTML::Table::Foot class. The class is a singleton
 # class, so we have to take extra measures to ensure that a fresh instance
 # is created between tests.
 ###############################################################################
 require 'rspec'
 require 'html/table'
 
-#####################################################################
-# Ensure that a fresh instance of Table::Foot is used between tests
-# by calling 'refresh' in the 'teardown' method.
-#####################################################################
-RSpec.describe HTML::Table::Foot do
-  let(:foot) do
-    Class.new(described_class) do
-      def self.refresh
-        @@foot = nil
-      end
-    end
+class HTML::Table::Foot
+  private
+  def refresh
+    @@foot = nil
   end
+end
 
+RSpec.describe HTML::Table::Foot do
   before do
     @table = HTML::Table.new
-    @tfoot = HTML::Table::Foot.create
-    StructuredWarnings::BuiltInWarning.disable{ foot.refresh }
+    @tfoot = described_class.create
   end
 
   example "new_not_allowed" do
-    expect{ HTML::Table::Foot.new }.to raise_error(NoMethodError)
+    expect{ described_class.new }.to raise_error(NoMethodError)
   end
 
   example "constructor" do
-    expect{ HTML::Table::Foot.create }.not_to raise_error
-    expect{ HTML::Table::Foot.create("foo") }.not_to raise_error
-    expect{ HTML::Table::Foot.create(1) }.not_to raise_error
-    expect{ HTML::Table::Foot.create(%w/foo bar baz/) }.not_to raise_error
-    expect{ HTML::Table::Foot.create([1,2,3]) }.not_to raise_error
-    expect{ HTML::Table::Foot.create([[1,2,3], ["foo","bar"]]) }.not_to raise_error
+    expect{ described_class.create }.not_to raise_error
+    expect{ described_class.create("foo") }.not_to raise_error
+    expect{ described_class.create(1) }.not_to raise_error
+    expect{ described_class.create(%w/foo bar baz/) }.not_to raise_error
+    expect{ described_class.create([1,2,3]) }.not_to raise_error
+    expect{ described_class.create([[1,2,3], ["foo","bar"]]) }.not_to raise_error
   end
 
   example "basic" do
@@ -46,14 +40,14 @@ RSpec.describe HTML::Table::Foot do
   end
 
   example "end_tags" do
-    expect(HTML::Table::Foot).to respond_to(:end_tags?)
-    expect(HTML::Table::Foot).to respond_to(:end_tags=)
-    expect{ HTML::Table::Foot.end_tags? }.not_to raise_error
-    expect{ HTML::Table::Foot.end_tags = true }.not_to raise_error
+    expect(described_class).to respond_to(:end_tags?)
+    expect(described_class).to respond_to(:end_tags=)
+    expect{ described_class.end_tags? }.not_to raise_error
+    expect{ described_class.end_tags = true }.not_to raise_error
   end
 
   example "end_tags_expected_errors" do
-    expect{ HTML::Table::Foot.end_tags = "foo" }.to raise_error(HTML::Mixin::StrongTyping::ArgumentTypeError)
+    expect{ described_class.end_tags = "foo" }.to raise_error(HTML::Mixin::StrongTyping::ArgumentTypeError)
   end
 
   example "with_attributes" do
@@ -65,14 +59,14 @@ RSpec.describe HTML::Table::Foot do
 
   example "push_single_row" do
     html = "<tfoot><tr><td>test</td></tr></tfoot>"
-    @tfoot.push HTML::Table::Row.new{ |r| r.content = "test" }
+    @tfoot.push Table::Row.new{|r| r.content = "test"}
     expect(@tfoot.html.gsub(/\s{2,}|\n/, '')).to eq(html)
   end
 
   example "push_multiple_rows" do
     html = "<tfoot><tr><td>test</td></tr><tr><td>foo</td></tr></tfoot>"
-    r1 = HTML::Table::Row.new{ |r| r.content = "test" }
-    r2 = HTML::Table::Row.new{ |r| r.content = "foo" }
+    r1 = Table::Row.new{|r| r.content = "test"}
+    r2 = Table::Row.new{|r| r.content = "foo"}
     @tfoot.push r1, r2
     expect(@tfoot.html.gsub(/\s{2,}|\n/, '')).to eq(html)
   end
@@ -85,7 +79,8 @@ RSpec.describe HTML::Table::Foot do
 
   example "add_content_in_constructor" do
     html = "<tfoot><tr><td>hello</td><td>world</td></tr></tfoot>"
-    @tfoot = HTML::Table::Foot.create(["hello","world"])
+    @tfoot.send(:refresh)
+    @tfoot = described_class.create(["hello","world"])
     expect(@tfoot.html.gsub(/\s{2,}|\n+/, '')).to eq(html)
   end
 
@@ -99,5 +94,11 @@ RSpec.describe HTML::Table::Foot do
       data.nowrap = true
     }
     expect(@tfoot.html.gsub(/\s{2,}|\n+/, '')).to eq(html)
+  end
+
+  after do
+    @table = nil
+    @tfoot.send(:refresh)
+    @tfoot = nil
   end
 end
