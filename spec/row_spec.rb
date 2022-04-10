6682365c2f355999451f8dd5943baf11b69d8300
Daniel Berger <djberg96@gmail.com>
2022-04-10 08:54:46 -0400

    Update max example length, refactor some table row specs.

--- spec/row_spec.rb
+++ spec/row_spec.rb
@@ -32,13 +32,13 @@ RSpec.describe HTML::Table::Row do
     expect{ @trow.header = true }.not_to raise_error
   end
 
-  example 'with_attributes' do
+  example 'with attributes' do
     html = "<tr align='center'></tr>"
     @trow.align = 'center'
     expect(@trow.html.gsub(/\s{2,}|\n+/, '')).to eq(html)
   end
 
-  example 'index_assignment_constraints' do
+  example 'index assignment constraints' do
     expect{ @trow[0] = 'foo' }.to raise_error(ArgumentTypeError)
     expect{ @trow[0] = 1 }.to raise_error(ArgumentTypeError)
     expect{ @trow[0] = Table::Caption.new }.to raise_error(ArgumentTypeError)
@@ -46,7 +46,7 @@ RSpec.describe HTML::Table::Row do
     expect{ @trow[0] = Table::Row::Header.new }.not_to raise_error
   end
 
-  example 'push_constraints' do
+  example 'push constraints' do
     expect{ @trow.push(Table::Caption.new) }.to raise_error(ArgumentTypeError)
     expect{ @trow.push(nil) }.to raise_error(ArgumentTypeError)
     expect{ @trow.push('test') }.not_to raise_error
@@ -55,7 +55,7 @@ RSpec.describe HTML::Table::Row do
     expect{ @trow.push(Table::Row::Header.new) }.not_to raise_error
   end
 
-  example 'double_arrow_constraints' do
+  example 'double arrow constraints' do
     expect{ @trow << Table::Caption.new }.to raise_error(ArgumentTypeError)
     expect{ @trow << 'test' }.not_to raise_error
     expect{ @trow << 'test' << 'foo' }.not_to raise_error
@@ -63,19 +63,19 @@ RSpec.describe HTML::Table::Row do
     expect{ @trow << Table::Row::Header.new }.not_to raise_error
   end
 
-  example 'header_in_constructor' do
+  example 'header in constructor' do
     expect{ @trow = Table::Row.new('test', true) }.not_to raise_error
     html = '<tr><th>test</th></tr>'
     expect(@trow.html.gsub(/\s+/, '')).to eq(html)
   end
 
-  example 'push_single_data_element' do
+  example 'push single data element' do
     html = '<tr><td>hello</td></tr>'
-    @trow.push Table::Row::Data.new{ |d| d.content = 'hello' }
+    @trow.push(Table::Row::Data.new{ |d| d.content = 'hello' })
     expect(@trow.html.gsub(/\s{2,}|\n+/, '')).to eq(html)
   end
 
-  example 'push_multiple_data_element' do
+  example 'push multiple data element' do
     html = '<tr><td>hello</td><td>world</td></tr>'
     d1 = Table::Row::Data.new{ |d| d.content = 'hello' }
     d2 = Table::Row::Data.new{ |d| d.content = 'world' }
@@ -83,19 +83,19 @@ RSpec.describe HTML::Table::Row do
     expect(@trow.html.gsub(/\s{2,}|\n+/, '')).to eq(html)
   end
 
-  example 'add_content_directly' do
+  example 'add content directly' do
     html = '<tr><td>hello</td><td>world</td></tr>'
     @trow.content = 'hello', 'world'
     expect(@trow.html.gsub(/\s{2,}|\n+/, '')).to eq(html)
   end
 
-  example 'add_content_in_constructor' do
+  example 'add content in constructor' do
     html = '<tr><td>hello</td><td>world</td></tr>'
     @trow = Table::Row.new(%w[hello world])
     expect(@trow.html.gsub(/\s{2,}|\n+/, '')).to eq(html)
   end
 
-  example 'configure_column' do
+  example 'configure column' do
     html = "<tr><td>hello</td><td abbr='test' width=3 nowrap>world</td></tr>"
     @trow.content = 'hello', 'world'
     @trow.configure(1) do |d|
@@ -106,16 +106,19 @@ RSpec.describe HTML::Table::Row do
     expect(@trow.html.gsub(/\s{2,}|\n+/, '')).to eq(html)
   end
 
-  example 'unshift_constraints' do
+  example 'unshift does not allow invalid types' do
     expect{ @trow.unshift(Table::Caption.new) }.to raise_error(ArgumentTypeError)
     expect{ @trow.unshift(nil) }.to raise_error(ArgumentTypeError)
+  end
+
+  example 'unshift allows proper types' do
     expect{ @trow.unshift('test') }.not_to raise_error
     expect{ @trow.unshift(7) }.not_to raise_error
     expect{ @trow.unshift(Table::Row::Data.new) }.not_to raise_error
     expect{ @trow.unshift(Table::Row::Header.new) }.not_to raise_error
   end
 
-  example 'configure_error' do
+  example 'configure error' do
     expect{ @trow.configure(0, 0){} }.to raise_error(ArgumentError)
   end
 
@@ -126,11 +129,18 @@ RSpec.describe HTML::Table::Row do
     expect{ Table::Row.indent_level = 3 }.not_to raise_error
   end
 
-  example 'end_tags' do
+  example 'end_tags?' do
     expect(Table::Row).to respond_to(:end_tags?)
+    expect(Table::Row.end_tags?).to be(true)
+  end
+
+  example 'end_tags= basic functionality' do
     expect(Table::Row).to respond_to(:end_tags=)
+    expect{ Table::Row.end_tags = true }.not_to raise_error
+  end
+
+  example 'end_tags= rejects invalid values' do
     expect{ Table::Row.end_tags = 'foo' }.to raise_error(ArgumentTypeError)
     expect{ Table::Row.end_tags = 1 }.to raise_error(ArgumentTypeError)
-    expect{ Table::Row.end_tags = true }.not_to raise_error
   end
 end
