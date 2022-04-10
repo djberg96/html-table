eec68694549c6a3fa121c872c95c3b31c40debef
Daniel Berger <djberg96@gmail.com>
2022-04-10 13:45:49 -0400

    Refactor the head specs a bit.

--- spec/head_spec.rb
+++ spec/head_spec.rb
@@ -34,8 +34,17 @@ RSpec.describe HTML::Table::Head do
 
   example 'constructor' do
     expect{ described_class.create }.not_to raise_error
+  end
+
+  example 'constructor with string' do
     expect{ described_class.create('foo') }.not_to raise_error
+  end
+
+  example 'constructor with numeric' do
     expect{ described_class.create(1) }.not_to raise_error
+  end
+
+  example 'constructor with arrays' do
     expect{ described_class.create(%w[foo bar baz]) }.not_to raise_error
     expect{ described_class.create([1, 2, 3]) }.not_to raise_error
     expect{ described_class.create([[1, 2, 3], %w[foo bar]]) }.not_to raise_error
@@ -46,8 +55,12 @@ RSpec.describe HTML::Table::Head do
     expect(@thead.html.gsub(/\s{2,}|\n/, '')).to eq(html)
   end
 
-  example 'end_tags' do
+  example 'end_tags? basic functionality' do
     expect(described_class).to respond_to(:end_tags?)
+    expect(described_class.end_tags?).to be(true)
+  end
+
+  example 'end_tags= does not allow invalid types' do
     expect(described_class).to respond_to(:end_tags=)
     expect{ described_class.end_tags? }.not_to raise_error
     expect{ described_class.end_tags = true }.not_to raise_error
